aa517d867e7045dd573087feafff6f6792291684
Padmarajan <padmaraj@apdeops.com>
2024-02-23 19:41:32 +0530

    project init

---
+++ src/main/java/com/web/app/StationaryWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationaryWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationaryWebApiApplication.class, args);
+	}
+
+}
